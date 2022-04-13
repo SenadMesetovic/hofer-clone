78ce70770eb5a56a4873309705ff99203be794db
Erniskucuk <66641659+Erniskucuk@users.noreply.github.com>
2022-04-13 11:06:33 +0200

    adding down site on click JS

---
+++ navbar.js
@@ -0,0 +1,11 @@
+document.querySelector("#downMenu").addEventListener("click", function(){
+var showSite = document.getElementById("menu");
+if(showSite.style.display == "none"){
+    showSite.style.display = "flex";
+} else{
+    showSite.style.display = "none";
+}
+});
+
+
+
