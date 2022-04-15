75ffad1d20381c19a977a1e18dcd399dacbdc8d4
Erniskucuk <66641659+Erniskucuk@users.noreply.github.com>
2022-04-15 17:46:48 +0200

    initial commit navbar

--- navbar.js
+++ navbar.js
@@ -2,8 +2,10 @@ document.querySelector("#downMenu").addEventListener("click", function(){
 var showSite = document.getElementById("menu");
 if(showSite.style.display == "none"){
     showSite.style.display = "flex";
+    document.getElementById("down").style.transform = 'rotate(180deg)';
 } else{
     showSite.style.display = "none";
+    document.getElementById("down").style.transform = 'rotate(360deg)';
 }
 });
 
